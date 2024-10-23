2e3e7e58a59efbe27ad09988566875589155f81c
Erik <slmv_e@icloud.com>
2024-10-23 05:16:31 +0300

    added raylib and created cmakelists

--- src/main.c
+++ src/main.c
@@ -1,7 +1,17 @@
-#include <stdio.h>
+#include "raylib.h"
 
 int main(void) {
-    printf("Hello, world!\n");
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+    InitWindow(screenWidth, screenHeight, "Raylib basic window");
+    SetTargetFPS(60);
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("It works!", 20, 20, 20, BLACK);
+        EndDrawing();
+    }
+    CloseWindow();
 
     return 0;
 }
